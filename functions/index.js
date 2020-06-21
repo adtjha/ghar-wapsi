5fade44ebc67b427ff66c0eb010623c0a427a540
adtjha <adtjha1@gmail.com>
2020-06-21 15:45:43 +0530

    Outline

--- functions/index.js
+++ functions/index.js
@@ -10,3 +10,17 @@ const functions = require('firebase-functions');
 exports.yeah = functions.https.onRequest((request, response) => {
     response.send("Hell Yeah !");
 });
+
+
+exports.AddEntry = functions.https.onRequest((req, res) => {
+    var id = req.body.id;
+    if(onVerify(id)){
+        res.send('Add entry to Database, source has been verified, also run few other Functions.');
+    } else {
+        res.send('Invalid');
+    }
+});
+
+onVerify(id){
+    return true;
+}
